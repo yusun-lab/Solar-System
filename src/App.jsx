e8cad01104584d51bb3f5aee8af3640dd0e98be2
yusun-lab <yu.sun@hyperisland.se>
2025-06-05 17:28:26 +0200

    feat: create AmbientLight component and adjust intensity
    - Create AmbientLight component
    - Set ambient light intensity to 3
    - Optimize component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sky from './components/Sky/Sky';
 import Sun from './components/Planets/00. Sun/Sun';
+import AmbientLight from './components/Lights/AmbientLight';
 import './App.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <a-scene>
         <Sky />
         <Sun />
-        <a-light type="ambient" intensity="0.5"></a-light>
+        <AmbientLight />
         <a-entity camera look-controls wasd-controls position="0 0 10"></a-entity>
       </a-scene>
     </div>
---
+++ src/components/Lights/AmbientLight.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AmbientLight = () => {
+  return (
+    <a-light
+      type="ambient"
+      intensity="3"
+    />
+  );
+};
+
+export default AmbientLight;
