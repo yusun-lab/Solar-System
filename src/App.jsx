2446aa3e43442488d20b2878fbcaddf2aa655a42
yusun-lab <yu.sun@hyperisland.se>
2025-06-05 17:03:20 +0200

    feat: add Sun component and adjust lighting
    - Create Sun component with texture
    - Add sun rotation animation
    - Adjust ambient light intensity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Sky from './components/Sky/Sky';
+import Sun from './components/Planets/00. Sun/Sun';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="app">
       <a-scene>
         <Sky />
+        <Sun />
         <a-light type="ambient" intensity="0.5"></a-light>
         <a-entity camera look-controls wasd-controls position="0 0 10"></a-entity>
       </a-scene>
---
+++ src/components/Planets/00. Sun/Sun.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import sunTexture from '../../../assets/images/00. Sun.jpg';
+
+const Sun = () => {
+  return (
+    <a-sphere
+      src={sunTexture}
+      position="0 0 0"
+      radius="5"
+      rotation="0 0 0"
+      animation="property: rotation; to: 0 360 0; loop: true; dur: 10000; easing: linear"
+    />
+  );
+};
+
+export default Sun;
