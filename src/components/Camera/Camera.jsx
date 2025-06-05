a4249eb0dbc0f9cd946dc647a04bd2c4a8b73834
yusun-lab <yu.sun@hyperisland.se>
2025-06-05 17:35:46 +0200

    feat: create ambient light component

---
+++ src/components/Camera/Camera.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Camera1 = () => {
+  return (
+    <a-entity
+      camera
+      look-controls
+      wasd-controls
+      position="0 0 10"
+    />
+  );
+};
+
+export default Camera1;
