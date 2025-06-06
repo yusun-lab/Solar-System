ae09a162e03b2581de03df68f4b9923689613e86
yusun-lab <yu.sun@hyperisland.se>
2025-06-06 18:33:58 +0200

    fix Mercury orbital animation

--- src/components/Planets/01. Mercury/Mercury.jsx
+++ src/components/Planets/01. Mercury/Mercury.jsx
@@ -3,16 +3,21 @@ import mercuryModel from './mercury.glb';
 
 const Mercury = () => {
   return (
-    <>      
-      {/* Mercury planet */}
+    <a-entity>
+      {/* Orbital container */}
       <a-entity
-        position="15 0 0"
-        scale="0.8 0.8 0.8"
-        rotation="0 0 0"
-        animation="property: rotation; to: 0 360 0; loop: true; dur: 5000; easing: linear"
-        gltf-model={mercuryModel}
-      />
-    </>
+        position="0 0 0"
+        animation="property: rotation; to: 0 360 0; loop: true; dur: 88000; easing: linear"
+      >
+        {/* Mercury with self rotation */}
+        <a-entity
+          position="15 0 0"
+          scale="0.8 0.8 0.8"
+          animation="property: rotation; to: 0 360 0; loop: true; dur: 5000; easing: linear"
+          gltf-model={mercuryModel}
+        />
+      </a-entity>
+    </a-entity>
   );
 };
 
