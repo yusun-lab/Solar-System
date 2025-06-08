76788950350c25dd819d7cafa46ce8c48c9eaba6
yusun-lab <yu.sun@hyperisland.se>
2025-06-08 18:21:01 +0200

    feat: add Mars component with orbiral parameters and adjust scale

---
+++ src/components/Planets/04. Mars/Mars.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import marsModel from '../../../assets/models/04. Mars.glb';
+
+const Mars = () => {
+  return (
+    <a-entity 
+      position="0 0 0"
+      animation="property: rotation; to: 0 360 0; loop: true; dur: 68700; easing: linear">
+      <a-entity
+        rotation="1.85 0 0"
+        animation="property: rotation; to: 0 360 0; loop: true; dur: 68700; easing: linear">
+        <a-entity
+          position="7.6 0 0"
+          scale="0.09 0.09 0.09"
+          animation="property: rotation; to: 0 360 0; loop: true; dur: 1030; easing: linear"
+          gltf-model={marsModel}
+        >
+        </a-entity>
+      </a-entity>
+    </a-entity>
+  );
+};
+
+export default Mars;
