23dffe0ff8fcce39331e4e3efb959b3ab9963a1a
yusun-lab <yu.sun@hyperisland.se>
2025-06-13 14:02:04 +0200

    feat: add green cursor to camera

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -7,7 +7,15 @@ const Camera = () => {
       look-controls
       wasd-controls
       position="0 0 100"
-    />
+    >
+      <a-cursor 
+        color="#4CAF50"
+        fuse="true"
+        fuse-timeout="1500"
+        geometry="primitive: ring; radiusInner: 0.008; radiusOuter: 0.012"
+        position="0 0 -1"
+      />
+    </a-entity>
   );
 };
 
